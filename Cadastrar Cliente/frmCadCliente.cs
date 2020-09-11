372405bffc2941ed94469bd5fec38ca4a5a9c1fe
Victor Augusto <vdbernardes@gmail.com>
2020-09-11 11:42:43 -0300

    Adicionado cadastrar clientes

---
+++ Cadastrar Cliente/frmCadCliente.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Cadastrar_Cliente
+{
+    public partial class frmCadCliente : Form
+    {
+
+        MySqlConnection conexao;
+        MySqlCommand comando;
+        MySqlDataAdapter da;
+        MySqlDataReader dr;
+        string strSQL;
+        public frmCadCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_cancelar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_cadastrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new MySqlConnection("Server=localhost;Database=bd_telesync;Uid=root;Pwd=;");
+
+                strSQL = "INSERT INTO CAD_CLIENTE (CPF, NOME, NOMEMAE, SEXO, EMAIL, BAIRRO, CEP, LOGRADOURO, NUMERO, UF, CIDADE, COMPLEMENTO, SENHA) VALUE (@CPF, @NOME, @NOMEMAE, @SEXO, @EMAIL, @BAIRRO, @CEP, @LOGRADOURO, @NUMERO, @UF, @CIDADE, @COMPLEMENTO, @SENHA)";
+
+                comando = new MySqlCommand(strSQL, conexao);
+                comando.Parameters.AddWithValue("@CPF", txtCPF.Text);
+                comando.Parameters.AddWithValue("@NOME", txtNome.Text);
+                comando.Parameters.AddWithValue("@NOMEMAE", txtNomemae.Text);
+                comando.Parameters.AddWithValue("@SEXO", txtSexo.Text);
+                comando.Parameters.AddWithValue("@EMAIL", txtEmail.Text);
+                comando.Parameters.AddWithValue("@BAIRRO", txtBairro.Text);
+                comando.Parameters.AddWithValue("@CEP", txtCEP.Text);
+                comando.Parameters.AddWithValue("@LOGRADOURO", txtLogradouro.Text);
+                comando.Parameters.AddWithValue("@NUMERO", txtNumero.Text);
+                comando.Parameters.AddWithValue("@UF", txtUF.Text);
+                comando.Parameters.AddWithValue("@CIDADE", txtCidade.Text);
+                comando.Parameters.AddWithValue("@COMPLEMENTO", txtComplemento.Text);
+                comando.Parameters.AddWithValue("@SENHA", txtSenha.Text);
+
+                conexao.Open();
+
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comando = null;
+            }
+        }
+
+        private void btnPular_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ Tela de Login/frmTelaLogin.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Tela_de_Login
+{
+    partial class frmTelaLogin
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnLogar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(23, 51);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(213, 22);
+            this.txtEmail.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Senha";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(54, 96);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(136, 22);
+            this.txtSenha.TabIndex = 2;
+            // 
+            // btnLogar
+            // 
+            this.btnLogar.Location = new System.Drawing.Point(84, 138);
+            this.btnLogar.Name = "btnLogar";
+            this.btnLogar.Size = new System.Drawing.Size(75, 56);
+            this.btnLogar.TabIndex = 4;
+            this.btnLogar.Text = "Login";
+            this.btnLogar.UseVisualStyleBackColor = true;
+            this.btnLogar.Click += new System.EventHandler(this.btnLogar_Click);
+            // 
+            // frmTelaLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 212);
+            this.Controls.Add(this.btnLogar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmail);
+            this.Name = "frmTelaLogin";
+            this.Text = "Tela de Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnLogar;
+    }
+}
+
