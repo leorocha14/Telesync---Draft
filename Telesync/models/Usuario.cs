3345aba27b3e5b5f49b82035f224f316972ed225
Victor Augusto <vdbernardes@gmail.com>
2020-09-12 11:56:31 -0300

    Alteração do nome da classe cliente para usuario

---
+++ Telesync/models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Telesync.models
+{
+    class Usuario
+    {
+
+        MySqlConnection conexao;
+        MySqlCommand comando;
+        MySqlDataAdapter da;
+        MySqlDataReader dr;
+        string strSQL;
+
+        public class conexão
+        {
+
+        }
+    }
+}
