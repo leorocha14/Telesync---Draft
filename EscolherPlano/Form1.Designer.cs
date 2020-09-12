a935c388ab3cdaccc324113e62a26e4c327dada7
Mauro Vieira de Morais <maurovmorais@gmail.com>
2020-09-12 19:26:54 -0300

    Add codigo Escolher Plano
    Falta finalizar código

--- EscolherPlano/Form1.Designer.cs
+++ EscolherPlano/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EscolherPlano
 {
-    partial class Form1
+    partial class F_EscolherPlano
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -31,17 +31,21 @@
             this.lt_planos = new System.Windows.Forms.ListView();
             this.col_plano = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.col_vlrUnitario = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.cb_lista_opcao = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.cb_planoTelefonia = new System.Windows.Forms.ComboBox();
+            this.cb_qtdeChips = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.btn_adcionar = new System.Windows.Forms.Button();
             this.btn_remover = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.tb_vlrPlano = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.tb_vlrTotal = new System.Windows.Forms.TextBox();
+            this.cb_planoInternet = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tb_qtdeChips = new System.Windows.Forms.TextBox();
+            this.btn_comprar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lt_planos
@@ -50,9 +54,10 @@
             this.col_plano,
             this.col_vlrUnitario});
             this.lt_planos.HideSelection = false;
-            this.lt_planos.Location = new System.Drawing.Point(21, 109);
+            this.lt_planos.Location = new System.Drawing.Point(21, 179);
             this.lt_planos.Name = "lt_planos";
-            this.lt_planos.Size = new System.Drawing.Size(545, 250);
+            this.lt_planos.Scrollable = false;
+            this.lt_planos.Size = new System.Drawing.Size(545, 309);
             this.lt_planos.TabIndex = 0;
             this.lt_planos.UseCompatibleStateImageBehavior = false;
             this.lt_planos.View = System.Windows.Forms.View.Details;
@@ -67,38 +72,35 @@
             this.col_vlrUnitario.Text = "Valor Unitário";
             this.col_vlrUnitario.Width = 400;
             // 
-            // cb_lista_opcao
+            // cb_planoTelefonia
             // 
-            this.cb_lista_opcao.FormattingEnabled = true;
-            this.cb_lista_opcao.Items.AddRange(new object[] {
+            this.cb_planoTelefonia.FormattingEnabled = true;
+            this.cb_planoTelefonia.Items.AddRange(new object[] {
             "",
             "Plano Telefone Controle Smart",
             "Plano Telefone Sem Limites",
-            "Plano Familar 4 linhas Sem limites",
-            "Pacote Internet 2Gb",
-            "Pacote Internet 4 Gb",
-            "Pacote Internet 6 Gb",
-            "Pacote Internet 10 Gb",
-            "Pacote Internet Compartilhado 20 Gb"});
-            this.cb_lista_opcao.Location = new System.Drawing.Point(21, 47);
-            this.cb_lista_opcao.Name = "cb_lista_opcao";
-            this.cb_lista_opcao.Size = new System.Drawing.Size(422, 24);
-            this.cb_lista_opcao.TabIndex = 1;
+            "Plano Familar 4 linhas Sem limites"});
+            this.cb_planoTelefonia.Location = new System.Drawing.Point(21, 47);
+            this.cb_planoTelefonia.Name = "cb_planoTelefonia";
+            this.cb_planoTelefonia.Size = new System.Drawing.Size(422, 24);
+            this.cb_planoTelefonia.TabIndex = 1;
+            this.cb_planoTelefonia.SelectedIndexChanged += new System.EventHandler(this.btn_adcionar_Click);
             // 
-            // comboBox2
+            // cb_qtdeChips
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Items.AddRange(new object[] {
+            this.cb_qtdeChips.FormattingEnabled = true;
+            this.cb_qtdeChips.Items.AddRange(new object[] {
             "",
             "1",
             "2",
             "3",
             "4",
             "5"});
-            this.comboBox2.Location = new System.Drawing.Point(460, 47);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(106, 24);
-            this.comboBox2.TabIndex = 2;
+            this.cb_qtdeChips.Location = new System.Drawing.Point(460, 47);
+            this.cb_qtdeChips.Name = "cb_qtdeChips";
+            this.cb_qtdeChips.Size = new System.Drawing.Size(106, 24);
+            this.cb_qtdeChips.TabIndex = 2;
+            this.cb_qtdeChips.SelectedIndexChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
@@ -112,36 +114,26 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(18, 23);
+            this.label2.Location = new System.Drawing.Point(22, 22);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 17);
+            this.label2.Size = new System.Drawing.Size(107, 17);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Tipo de Plano";
+            this.label2.Text = "Plano Telefonia";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(18, 80);
+            this.label3.Location = new System.Drawing.Point(18, 150);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(138, 17);
             this.label3.TabIndex = 5;
             this.label3.Text = "Planos selecionados";
             // 
-            // btn_adcionar
-            // 
-            this.btn_adcionar.Location = new System.Drawing.Point(586, 109);
-            this.btn_adcionar.Name = "btn_adcionar";
-            this.btn_adcionar.Size = new System.Drawing.Size(108, 23);
-            this.btn_adcionar.TabIndex = 6;
-            this.btn_adcionar.Text = "Escolher";
-            this.btn_adcionar.UseVisualStyleBackColor = true;
-            this.btn_adcionar.Click += new System.EventHandler(this.btn_adcionar_Click);
-            // 
             // btn_remover
             // 
-            this.btn_remover.Location = new System.Drawing.Point(586, 168);
+            this.btn_remover.Location = new System.Drawing.Point(586, 179);
             this.btn_remover.Name = "btn_remover";
-            this.btn_remover.Size = new System.Drawing.Size(108, 23);
+            this.btn_remover.Size = new System.Drawing.Size(133, 27);
             this.btn_remover.TabIndex = 7;
             this.btn_remover.Text = "Remover";
             this.btn_remover.UseVisualStyleBackColor = true;
@@ -152,7 +144,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label4.Location = new System.Drawing.Point(581, 215);
+            this.label4.Location = new System.Drawing.Point(581, 284);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(140, 25);
             this.label4.TabIndex = 8;
@@ -160,17 +152,19 @@
             // 
             // tb_vlrPlano
             // 
-            this.tb_vlrPlano.Location = new System.Drawing.Point(586, 257);
+            this.tb_vlrPlano.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_vlrPlano.Location = new System.Drawing.Point(584, 320);
             this.tb_vlrPlano.Name = "tb_vlrPlano";
-            this.tb_vlrPlano.Size = new System.Drawing.Size(108, 22);
+            this.tb_vlrPlano.Size = new System.Drawing.Size(135, 30);
             this.tb_vlrPlano.TabIndex = 9;
+            this.tb_vlrPlano.TextChanged += new System.EventHandler(this.tb_vlrPlano_TextChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label5.Location = new System.Drawing.Point(587, 294);
+            this.label5.Location = new System.Drawing.Point(581, 360);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(107, 25);
             this.label5.TabIndex = 10;
@@ -178,29 +172,91 @@
             // 
             // tb_vlrTotal
             // 
-            this.tb_vlrTotal.Location = new System.Drawing.Point(588, 337);
+            this.tb_vlrTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_vlrTotal.Location = new System.Drawing.Point(586, 391);
             this.tb_vlrTotal.Name = "tb_vlrTotal";
-            this.tb_vlrTotal.Size = new System.Drawing.Size(106, 22);
+            this.tb_vlrTotal.Size = new System.Drawing.Size(133, 30);
             this.tb_vlrTotal.TabIndex = 11;
             // 
-            // Form1
+            // cb_planoInternet
+            // 
+            this.cb_planoInternet.FormattingEnabled = true;
+            this.cb_planoInternet.Items.AddRange(new object[] {
+            "Pacote Internet 2Gb",
+            "Pacote Internet 4 Gb",
+            "Pacote Internet 6 Gb",
+            "Pacote Internet 10 Gb",
+            "Pacote Internet Compartilhado 20 Gb"});
+            this.cb_planoInternet.Location = new System.Drawing.Point(21, 109);
+            this.cb_planoInternet.Name = "cb_planoInternet";
+            this.cb_planoInternet.Size = new System.Drawing.Size(422, 24);
+            this.cb_planoInternet.TabIndex = 12;
+            this.cb_planoInternet.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Plano Internet";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label7.Location = new System.Drawing.Point(583, 217);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 25);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Qtde Chips";
+            // 
+            // tb_qtdeChips
+            // 
+            this.tb_qtdeChips.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_qtdeChips.Location = new System.Drawing.Point(586, 248);
+            this.tb_qtdeChips.Name = "tb_qtdeChips";
+            this.tb_qtdeChips.Size = new System.Drawing.Size(133, 30);
+            this.tb_qtdeChips.TabIndex = 15;
+            this.tb_qtdeChips.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btn_comprar
+            // 
+            this.btn_comprar.BackColor = System.Drawing.Color.LimeGreen;
+            this.btn_comprar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_comprar.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_comprar.Location = new System.Drawing.Point(588, 453);
+            this.btn_comprar.Name = "btn_comprar";
+            this.btn_comprar.Size = new System.Drawing.Size(135, 35);
+            this.btn_comprar.TabIndex = 16;
+            this.btn_comprar.Text = "Comprar";
+            this.btn_comprar.UseVisualStyleBackColor = false;
+            // 
+            // F_EscolherPlano
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(740, 371);
+            this.ClientSize = new System.Drawing.Size(740, 509);
+            this.Controls.Add(this.btn_comprar);
+            this.Controls.Add(this.tb_qtdeChips);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cb_planoInternet);
             this.Controls.Add(this.tb_vlrTotal);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.tb_vlrPlano);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btn_remover);
-            this.Controls.Add(this.btn_adcionar);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.cb_lista_opcao);
+            this.Controls.Add(this.cb_qtdeChips);
+            this.Controls.Add(this.cb_planoTelefonia);
             this.Controls.Add(this.lt_planos);
-            this.Name = "Form1";
+            this.Name = "F_EscolherPlano";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Escolher Plano";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -210,19 +266,23 @@
         #endregion
 
         private System.Windows.Forms.ListView lt_planos;
-        private System.Windows.Forms.ComboBox cb_lista_opcao;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox cb_planoTelefonia;
+        private System.Windows.Forms.ComboBox cb_qtdeChips;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ColumnHeader col_plano;
         private System.Windows.Forms.ColumnHeader col_vlrUnitario;
-        private System.Windows.Forms.Button btn_adcionar;
         private System.Windows.Forms.Button btn_remover;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox tb_vlrPlano;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox tb_vlrTotal;
+        private System.Windows.Forms.ComboBox cb_planoInternet;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tb_qtdeChips;
+        private System.Windows.Forms.Button btn_comprar;
     }
 }
 
--- EscolherPlano/Form1.cs
+++ EscolherPlano/Form1.cs
@@ -7,30 +7,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace EscolherPlano
 {
-    public partial class Form1 : Form
+    public partial class F_EscolherPlano : Form
     {
-        public Form1()
+
+         public F_EscolherPlano()
         {
             InitializeComponent();
         }
 
         private void btn_adcionar_Click(object sender, EventArgs e)
         {
-            string[] pl = new string[2];
-            pl[0] = cb_lista_opcao.Text;
-            pl[1] = "";
 
-            ListViewItem lst = new ListViewItem(pl);
+            ComboBox cmb = (ComboBox)sender;
+            int selectedindex = cmb.SelectedIndex;
 
+            double[] vlrUnitTel = new double[3];
+            vlrUnitTel[0] = 123.10;
+            vlrUnitTel[1] = 300.10;
+            vlrUnitTel[2] = 400.10;
+            
+            string[] tel = new string[2];
+            tel[0] = cb_planoTelefonia.Text;
+            tel[1] = String.Format(CultureInfo.GetCultureInfo("pt-BR"),"{0:C}",vlrUnitTel[selectedindex]);
+            
+            ListViewItem lst = new ListViewItem(tel);
             lt_planos.Items.Add(lst);
         }
 
         private void btn_remover_Click(object sender, EventArgs e)
         {
             lt_planos.Items.Clear();
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            ComboBox cmb = (ComboBox)sender;
+            int selectedindex = cmb.SelectedIndex;
+
+            double[] vlrUnitInt = new double[5];
+            vlrUnitInt[0] = 223.10;
+            vlrUnitInt[1] = 500.10;
+            vlrUnitInt[2] = 700.10;
+            vlrUnitInt[3] = 800.10;
+            vlrUnitInt[4] = 900.10;
+
+            string[] net = new string[2];
+            net[0] = cb_planoInternet.Text;
+            net[1] = String.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", vlrUnitInt[selectedindex]);
+
+            ListViewItem lst = new ListViewItem(net);
+            lt_planos.Items.Add(lst);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            tb_qtdeChips.Text = cb_qtdeChips.SelectedItem.ToString();
+        }
+
+        private void tb_vlrPlano_TextChanged(object sender, EventArgs e)
+        {
+          
+            
         }
     }
 }
