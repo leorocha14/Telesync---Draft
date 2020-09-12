83186c88db1ffca3d8f783380ba7ee786eccaeba
Leonardo Rocha <leonardorochabarros@hotmail.com>
2020-09-12 12:45:38 -0300

    correcoes de merge

--- Telesync/repositories/UsuarioDao.cs
+++ Telesync/repositories/UsuarioDao.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
-using System.Data;
 
-namespace Telesync.models
+namespace Telesync.repositories
 {
     class UsuarioDao
     {
