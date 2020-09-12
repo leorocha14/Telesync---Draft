9d8453c3c37f11c7e3b5ebfae28f4c6aa314392a
Leonardo Rocha <leonardorochabarros@hotmail.com>
2020-09-12 13:18:34 -0300

    Link com cadastro de usuario

--- Telesync/views/Login.cs
+++ Telesync/views/Login.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Telesync
@@ -20,6 +13,12 @@ namespace Telesync
         private void LogarBtn_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            frmCadClientes cadastroForm = new frmCadClientes();
+            cadastroForm.Show();
         }
     }
 }
--- Telesync/views/frmCadClientes.cs
+++ Telesync/views/frmCadClientes.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
 
 namespace Telesync
 {
@@ -65,7 +58,7 @@ namespace Telesync
 
         private void btnPular_Click(object sender, EventArgs e)
         {
-
+            this.Dispose();
         }
 
         private void btn_cancelar_Click(object sender, EventArgs e)
