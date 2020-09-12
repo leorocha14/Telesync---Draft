b523cf9041feb38f30f8604dee7f7d5f08d37eb2
Leonardo Rocha <leonardorochabarros@hotmail.com>
2020-09-12 18:33:18 -0300

    implementando injecao de dependencia

---
+++ Telesync/config/ApplicationModule.cs
@@ -0,0 +1,18 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telesync.validations;
+
+namespace Telesync.config
+{
+    public class ApplicationModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind(typeof(IValidation)).To(typeof(UserValidation));
+        }
+    }
+}
--- Telesync/views/Login.cs
+++ Telesync/views/Login.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Windows.Forms;
+using Telesync.validations;
 
 namespace Telesync
 {
     public partial class Login : Form
     {
-        public Login()
+        private IValidation _validation;
+
+        public Login(IValidation validation)
         {
             InitializeComponent();
+            this._validation = validation;
         }
 
         private void LogarBtn_Click(object sender, EventArgs e)
         {
-
+            _validation.validate();
         }
 
         private void btnCadastrar_Click(object sender, EventArgs e)
